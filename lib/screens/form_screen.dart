47935c66360c1894d228075b44c06e2fd6e3df37
Luiz Felipe de Oliveira Rodrigues <luiz.123felip@gmail.com>
2023-03-15 20:33:20 -0300

    update form_screen

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -140,12 +140,12 @@ class _FormScreenState extends State<FormScreen> {
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: () {
+                    onPressed: () async {
                       if (_formKey.currentState!.validate()) {
                         // print(nameController.text);
                         // print(int.parse(difficultyController.text));
                         // print(imageController.text);
-                        TaskDao().save(Task(
+                        await TaskDao().save(Task(
                           nameController.text,
                           imageController.text,
                           int.parse(difficultyController.text)));
